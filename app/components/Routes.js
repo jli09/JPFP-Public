a7706b5c268950004a0b2afbb4a863642da8b15e
jli09 <56615820+jli09@users.noreply.github.com>
2020-03-30 17:02:49 -0400

    Navbar set up

--- app/components/Routes.js
+++ app/components/Routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { AllCampuses } from './AllCampuses';
 import { AllStudents } from './AllStudents';
+import Navbar from './Navbar';
 
 const Routes = () => {
   return (
     <Router>
       <div>
-        <nav>Welcome!</nav>
+        <Navbar />
         <main>
           <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
           <Route path="/campuses" component={AllCampuses} />
