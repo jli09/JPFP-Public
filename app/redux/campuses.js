43e7afcff762da14ebeb7468e4122c33a01071e9
jli09 <56615820+jli09@users.noreply.github.com>
2020-03-30 17:40:38 -0400

    created subreducers for campuses and students

--- app/redux/campuses.js
+++ app/redux/campuses.js
@@ -1,9 +1,27 @@
-export const setCampuses = () => {};
+const SET_CAMPUSES = 'SET_CAMPUSES';
 
-export const fetchCampuses = () => {};
+export const setCampuses = campuses => ({
+  type: SET_CAMPUSES,
+  campuses,
+});
+
+export const fetchCampuses = () => async (dispatch, getState, axios) => {
+  try {
+    const { data } = await axios.get('/api/campuses/');
+    dispatch(setCampuses(data));
+  } catch (err) {
+    console.log('Could not get campuses from database :-(');
+    console.error(err.stack);
+  }
+};
 
 // Take a look at app/redux/index.js to see where this reducer is
 // added to the Redux store with combineReducers
-export default function campusesReducer() {
-  return null;
+export default function campusesReducer(campuses = [], action) {
+  switch (action.type) {
+    case SET_CAMPUSES:
+      return action.campuses;
+    default:
+      return campuses;
+  }
 }
--- app/redux/students.js
+++ app/redux/students.js
@@ -1,9 +1,28 @@
-export const setStudents = () => {};
+const SET_STUDENTS = 'SET_STUDENTS';
 
-export const fetchStudents = () => {};
+export const setStudents = (students) => ({
+  type: SET_STUDENTS, 
+  students
+ });
+
+export const fetchStudents = () => async (dispatch, getState, axios) => {
+  try {
+    const { data } = await axios.get('/api/students/');
+    dispatch(setStudents(data));
+  }
+  catch (err) {
+    console.log('Could not get students from database :-(');
+    console.error(err.stack);
+  }
+};
 
 // Take a look at app/redux/index.js to see where this reducer is
 // added to the Redux store with combineReducers
-export default function studentsReducer() {
-  return null;
+export default function studentsReducer(students = [], action) {
+  switch (action.type) {
+    case SET_STUDENTS:
+      return action.students;
+    default:
+      return students;
+  }
 }
