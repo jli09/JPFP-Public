315c620894b2d04a664d2975adc465e2fdc061a1
jli09 <56615820+jli09@users.noreply.github.com>
2020-03-31 15:18:26 -0400

    SingleCampus component campus portion basic css styling completed

--- app/components/SingleCampus.js
+++ app/components/SingleCampus.js
@@ -3,31 +3,39 @@ import { fetchSingleCampus } from '../redux/singleCampus';
 import { connect } from 'react-redux';
 
 class SingleCampus extends React.Component {
-    componentDidMount() {
-        const id = this.props.match.params.campusId;
-        this.props.getSingleCampus(id);
-    }
+  componentDidMount() {
+    const id = this.props.match.params.campusId;
+    this.props.getSingleCampus(id);
+  }
 
-    render() {
-        const { campus } = this.props;
+  render() {
+    const { campus } = this.props;
 
-        return (
-            <div>
-                <img src={campus.imageUrl} />
-                <p>{campus.name}</p>
-                <p>{campus.address}</p>
-                <p>{campus.description}</p>
-            </div>
-        )
-    }
+    return (
+      <div className="single_campus_container">
+        <div className="single_campus_display">
+          <div className="single_campus_left">
+            <img src={campus.imageUrl} className="single_campus_img" />
+            <h5>{campus.address}</h5>
+          </div>
+          <div className="single_campus_right">
+            <h2>{campus.name}</h2>
+            <p>{campus.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => ({
-    campus: state.singleCampus
+const mapStateToProps = state => ({
+  campus: state.singleCampus,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    getSingleCampus: (id) => { (dispatch(fetchSingleCampus(id))) }
+const mapDispatchToProps = dispatch => ({
+  getSingleCampus: id => {
+    dispatch(fetchSingleCampus(id));
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleCampus);
