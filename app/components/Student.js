3d0d9ee119f29d61c20a21e3cc8e9dbd556ef18a
jli09 <56615820+jli09@users.noreply.github.com>
2020-03-31 16:34:03 -0400

    Links to SingleStudent added to each Student in AllStudents view

--- app/components/Student.js
+++ app/components/Student.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Student = props => {
   const { student } = props;
 
   return (
-    <div className="student_display">
+    <Link to={`/students/${student.id}`}>
+      <div className="student_display">
         <img src={student.imageUrl} className="student_img" />
-          <h3>{student.firstName} {student.lastName}</h3>
-          <p>{student.campus ? student.campus.name : 'no campus'}</p>
-    </div>
+        <h3>
+          {student.firstName} {student.lastName}
+        </h3>
+        <p>{student.campus ? student.campus.name : 'no campus'}</p>
+      </div>
+    </Link>
   );
 };
 
