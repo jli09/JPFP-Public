d3d31da445a2e774819146a932c5d8cc066bc3c7
jli09 <56615820+jli09@users.noreply.github.com>
2020-03-31 12:25:33 -0400

    added api route for single campus

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -1,12 +1,24 @@
 const router = require('express').Router();
-const { Campus } = require('../db');
+const { Campus, Student } = require('../db');
 
 router.get('/', async (req, res, next) => {
   try {
     const campuses = await Campus.findAll();
     res.json(campuses);
   } catch (err) {
-    console.error(err.stack);
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const singleCampus = await Campus.findOne({
+      where: { id: req.params.id },
+      include: [{ model: Student }]
+    });
+    res.json(singleCampus);
+  } catch (err) {
+    next(err);
   }
 });
 
