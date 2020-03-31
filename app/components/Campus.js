1306422cbb2fb1e21c014888ed045c07112ee96d
jli09 <56615820+jli09@users.noreply.github.com>
2020-03-31 15:44:05 -0400

    SingleCampus Links added to AllCampuses component

--- app/components/Campus.js
+++ app/components/Campus.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Campus = props => {
   const { campus } = props;
 
   return (
-    <div className="campus_display">
-      <div className="campus_display_left">
-        <img src={campus.imageUrl} className="campus_img" />
+    <Link to={`/campuses/${campus.id}`}>
+      <div className="campus_display">
+        <div className="campus_display_left">
+          <img src={campus.imageUrl} className="campus_img" />
+        </div>
+        <div className="campus_display_right">
+          <h2>{campus.name}</h2>
+          <h3>{campus.address}</h3>
+          <p>{campus.description}</p>
+        </div>
       </div>
-      <div className="campus_display_right">
-              <h2>{campus.name}</h2>
-              <h3>{campus.address}</h3>
-              <p>{campus.description}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
